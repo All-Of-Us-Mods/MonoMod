d41aaf9fd9a552ac5bc16c58701c6ca8c975019d
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-08-14 09:19:53 -0400

    fix compile issues with android system

--- src/MonoMod.Core/Platforms/Systems/AndroidSystem.cs
+++ src/MonoMod.Core/Platforms/Systems/AndroidSystem.cs
@@ -5,10 +5,8 @@ using MonoMod.Utils;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Linq;
 using System.Reflection;
 
 namespace MonoMod.Core.Platforms.Systems
@@ -22,9 +20,20 @@ namespace MonoMod.Core.Platforms.Systems
         private readonly Abi defaultAbi;
         public Abi? DefaultAbi => defaultAbi;
 
+        public IEnumerable<LoadedModule> EnumerateLoadedModules()
+        {
+            foreach (var module in Linux.Procfs.ParseMapsModules(Linux.Procfs.ProcPid.Self)!)
+            {
+                yield return new LoadedModule(module.StartAddress, module.Path, module.Size);
+            }
+        }
+
         public IEnumerable<string?> EnumerateLoadedModuleFiles()
         {
-            return Process.GetCurrentProcess().Modules.Cast<ProcessModule>().Select(m => m.FileName)!;
+            foreach (var module in EnumerateLoadedModules())
+            {
+                yield return module.FileName;
+            }
         }
 
         private readonly nint PageSize;
